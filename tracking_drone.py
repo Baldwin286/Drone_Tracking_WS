36642efc14ba82b2844a0384075b22c70a7992fc
PhamThanhBien <thanhbienpham2003@gmail.com>
2025-09-21 20:34:18 +0700

    Update tracking

--- tracking_drone.py
+++ tracking_drone.py
@@ -29,7 +29,6 @@ def connectMyCopter():
   print("GPS: %s" % vehicle.gps_0) 
   print("Groundspeed: %s" % vehicle.groundspeed)
   print("Airspeed: %s" % vehicle.airspeed)
-  print("Gimbal status: %s" % vehicle.gimbal)
   print("Battery: %s" % vehicle.battery)
   print("EKF OK?: %s" % vehicle.ekf_ok)
   print("Last Heartbeat: %s" % vehicle.last_heartbeat)
@@ -38,9 +37,9 @@ def connectMyCopter():
   print("Rangefinder voltage: %s" % vehicle.rangefinder.voltage)
   print("Heading: %s" % vehicle.heading)
   print("Is Armable?: %s" % vehicle.is_armable)
-  print("System status: %s" % vehicle.system_status.state)
+  print( "System status: %s" % vehicle.system_status.state)
   print("Mode: %s" % vehicle.mode.name)    # settable
-  print("Armed: %s" % vehicle.armed)       # settable
+  print("Armed: %s" % vehicle.armed)      # settable
   return  vehicle
   
 

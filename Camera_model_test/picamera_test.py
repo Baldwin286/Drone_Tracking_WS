3439bf7cdd96254a17ec5c92b9197dd801cc1022
PhamThanhBien <thanhbienpham2003@gmail.com>
2025-11-15 19:34:31 +0700

    test camera

--- Camera_model_test/picamera_test.py
+++ Camera_model_test/picamera_test.py
@@ -1,69 +1,26 @@
-# from picamera2 import Picamera2
-# import cv2
-# from flask import Flask, Response
-
-# app = Flask(__name__)
-
-# # Initialize camera
-# picam2 = Picamera2()
-# config = picam2.create_video_configuration(main={"size": (640, 360)})
-# picam2.configure(config)
-# picam2.start()
-
-# autofocus_on = True
-# def generate():
-#     while True:
-#         request = picam2.capture_request()
-#         frame = request.make_array("main")
-#         request.release()
-
-#         # Correct color for OpenCV
-#         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-#         # Encode JPEG with lower quality (faster)
-#         ret, buffer = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 70])
-#         if not ret:
-#             continue
-
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
-
-# @app.route('/video')
-# def video_feed():
-#     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-# @app.route('/')
-# def index():
-#     return "<h1>Drone Camera Stream</h1><img src='/video' width='640'>"
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000, threaded=True)
 from picamera2 import Picamera2
 import cv2
 from flask import Flask, Response
 
 app = Flask(__name__)
 
-# --- Initialize camera ---
+# Initialize camera
 picam2 = Picamera2()
 config = picam2.create_video_configuration(main={"size": (640, 360)})
 picam2.configure(config)
-
-
-picam2.set_controls({
-    "AfMode": 0,          
-    "LensPosition": 0.2   
-})
-
 picam2.start()
 
+autofocus_on = True
 def generate():
     while True:
         request = picam2.capture_request()
         frame = request.make_array("main")
         request.release()
 
+        # Correct color for OpenCV
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+        # Encode JPEG with lower quality (faster)
         ret, buffer = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 70])
         if not ret:
             continue
@@ -71,17 +28,15 @@ def generate():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
 
-
 @app.route('/video')
 def video_feed():
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
 @app.route('/')
 def index():
-    return "<h1>🚁 Drone Camera Stream (AF OFF)</h1><img src='/video' width='640'>"
-
+    return "<h1>Drone Camera Stream</h1><img src='/video' width='640'>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True)
+
 
